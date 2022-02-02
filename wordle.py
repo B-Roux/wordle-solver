207ee20ef08f73a464d909e760429f6298cdb2fd
bgeroux <B-Roux@users.noreply.github.com>
2022-02-02 12:28:40 -0600

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,13 @@
+import os
+os.chdir('C:/Users/baren/Documents/tmp')
+
+def print_wordle_matches (word_builder, in_word, not_in_word):
+    with open('wordle_words.txt', 'r') as dictionary:
+        for word in dictionary:
+            word = word.strip('\n')
+            if    any([char not in word for char in in_word]): continue
+            elif  any([char in word for char in not_in_word]): continue
+            elif  any([pair[0] != '_' and pair[0] != pair[1] for pair in zip(word_builder, word)]): continue
+            else: print(word.upper())
+
+print_wordle_matches ('_oist', 'ts', 'whxenad')
